319a21f3f81a35ab5a717093a8418443d5d1d408
alicia7818 <95030087+alicia7818@users.noreply.github.com>
2023-04-24 11:03:53 +0200

    appli mobile

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import ProfilePage from './ProfilPage';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
 
 const logoUrl = './../assets/logo1.png';
 
